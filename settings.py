37270115c0cc6892e7124412abbf3edf054e9755
ilev4ik <ilev4ik@gmail.com>
2016-06-13 11:34:27 +0300

    Получение друзей друзей

---
+++ settings.py
@@ -0,0 +1,10 @@
+api_v = '5.21'
+token = ('a4e0606bc6c2210ea8ab8dea93f3172f68ab5340ba4a8dc635fef774e0f7fafd407df2c62387a750418b9')
+my_id = '174546672'
+deep = 1
+broker = {'user': '', 'password':'', 'ip':'', 'vhost':'vk_friends'}
+backend = broker
+max_workers = 1
+delay = 0.01
+m_friends_dct = {'file': 'mutual_friends_dct', 'graph':'mutual_friends_g'}
+d_friends_dct = {'file': 'deep_friends_dct', 'graph': 'deep_friends_g'}
